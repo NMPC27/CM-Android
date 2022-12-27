f208b97ed690ed86f5ec663a877d9a5f71684493
nunocunha <nunocunha@ua.pt>
2022-12-27 16:22:54 +0000

    fix git

--- app/src/main/java/com/example/cinem/MainActivity.java
+++ app/src/main/java/com/example/cinem/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.cinem;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import android.os.Bundle;
 
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
@@ -35,7 +38,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        createNotificationChannel();
         login();
+    }
+
+    private void createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("Cinem@", "Cinem@", NotificationManager.IMPORTANCE_DEFAULT);
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            NotificationManager notificationManager = getSystemService(NotificationManager.class);
+            notificationManager.createNotificationChannel(channel);
+        }
     }
 
     private void login() {
