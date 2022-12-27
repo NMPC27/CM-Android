91645c4af40e43388d6aece7fce6952acfbb1627
nunocunha <nunocunha@ua.pt>
2022-12-27 12:09:33 +0000

    -mapa done falta: -qrcode -fingerprint -scan qrcode -lock vertical position -notificaçoes

--- app/src/main/java/com/example/cinem/ui/map/MapFragment.java
+++ app/src/main/java/com/example/cinem/ui/map/MapFragment.java
@@ -1,16 +1,36 @@
 package com.example.cinem.ui.map;
 
+
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.cinem.R;
 import com.example.cinem.databinding.FragmentMapBinding;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapFragment extends Fragment {
 
@@ -18,15 +38,98 @@ public class MapFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        MapViewModel dashboardViewModel =
-                new ViewModelProvider(this).get(MapViewModel.class);
 
-        binding = FragmentMapBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
+        // Initialize view
+        View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        final TextView textView = binding.teste;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-        return root;
+        // Initialize map fragment
+        SupportMapFragment supportMapFragment = (SupportMapFragment)
+                getChildFragmentManager().findFragmentById(R.id.google_map);
+
+        // Async map
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                // When map is loaded
+                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(39.651020, -8.012924), 7));
+
+                googleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(38.694173, -8.941212))
+                        .title("Alegro Montijo"));
+                googleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(38.723206, -9.161943))
+                        .title("Amoreiras"));
+                googleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(38.737138, -9.399708))
+                        .title("CascaiShopping"));
+                googleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(41.284030, -8.360714))
+                        .title("Ferrara Plaza"));
+                googleMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(40.626954, -8.645659))
+                        .title("Glicinias"));
+
+                if (ContextCompat.checkSelfPermission(
+                        getActivity(),
+                        Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+                        && ContextCompat.checkSelfPermission(
+                        getActivity(),
+                        Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+
+                    if (!isLocationEnabled()) {
+                        Toast.makeText(getActivity(), "Please turn on" + " your location...", Toast.LENGTH_LONG).show();
+                        Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(intent);
+                    }
+
+                    googleMap.setMyLocationEnabled(true);
+                    Log.d("tag","GRANTTED");
+
+                } else {
+                    requestPermissions(
+                            new String[] {
+                                    Manifest.permission
+                                            .ACCESS_FINE_LOCATION,
+                                    Manifest.permission
+                                            .ACCESS_COARSE_LOCATION },
+                            100);
+                }
+
+
+
+            }
+        });
+        // Return view
+        return view;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String[] permissions,
+            @NonNull int[] grantResults){
+
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+        // Check condition
+        if (requestCode == 100 && (grantResults.length > 0)
+                && (grantResults[0] + grantResults[1]
+                == PackageManager.PERMISSION_GRANTED)) {
+            // When permission are granted
+            // Call  method
+            Log.d("tag","GRANTTED");
+        } else {
+            Toast.makeText(
+                    getActivity(),
+                            "Permission denied",
+                            Toast.LENGTH_SHORT)
+                    .show();
+        }
+    }
+
+    private boolean isLocationEnabled() {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
     }
 
     @Override
