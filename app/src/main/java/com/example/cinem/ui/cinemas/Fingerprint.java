1ac3f4dbe006ac957ffcb404aeb0de898028d8e7
nunocunha <nunocunha@ua.pt>
2022-12-27 19:12:03 +0000

    Fingerprint

--- app/src/main/java/com/example/cinem/ui/cinemas/Fingerprint.java
+++ app/src/main/java/com/example/cinem/ui/cinemas/Fingerprint.java
@@ -4,10 +4,17 @@ import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.biometric.BiometricManager;
+import androidx.biometric.BiometricPrompt;
+import androidx.core.content.ContextCompat;
 
 import com.example.cinem.R;
 import com.example.cinem.ui.ReminderBroadcast;
@@ -18,6 +25,7 @@ import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.TimeZone;
+import java.util.concurrent.Executor;
 
 
 public class Fingerprint extends AppCompatActivity {
@@ -30,7 +38,7 @@ public class Fingerprint extends AppCompatActivity {
     String email;
     double preco;
 
-    TextView fingerprint;
+    Button btn_fp,btn_fppin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,12 +56,47 @@ public class Fingerprint extends AppCompatActivity {
             preco = extras.getDouble("preco");
         }
 
-        fingerprint = findViewById(R.id.fingerprint);
-        fingerprint.setText(cinemaName+filmeId+hora+numTickets+email+preco);
+        btn_fp = findViewById(R.id.btn_fp);
+        btn_fppin = findViewById(R.id.btn_fppin);
 
+        checkBioMetricSupported();
 
-        insertDBticket();
-        notification();
+        Executor executor = ContextCompat.getMainExecutor(this);
+        BiometricPrompt biometricPrompt = new BiometricPrompt(Fingerprint.this,executor,new BiometricPrompt.AuthenticationCallback() {
+
+            @Override
+            public void onAuthenticationError(int errorCode, @NonNull CharSequence errString){
+                super.onAuthenticationError(errorCode,errString);
+                Toast.makeText(Fingerprint.this,"Auth error: "+ errString,Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result){
+                super.onAuthenticationSucceeded(result);
+                Toast.makeText(Fingerprint.this,"Auth succeeded",Toast.LENGTH_SHORT).show();
+                insertDBticket();
+                notification();
+            }
+            @Override
+            public void onAuthenticationFailed(){
+                super.onAuthenticationFailed();
+                Toast.makeText(Fingerprint.this,"Auth failed",Toast.LENGTH_SHORT).show();
+            }
+
+        });
+
+        // for fingerprint only
+        btn_fp.setOnClickListener(view->{
+            BiometricPrompt.PromptInfo.Builder promptInfo = dialogMetric();
+            promptInfo.setNegativeButtonText("Cancel");
+            biometricPrompt.authenticate(promptInfo.build());
+        });
+        // for button fingerprint or pattern or pin
+        btn_fppin.setOnClickListener(view -> {
+            BiometricPrompt.PromptInfo.Builder promptInfo = dialogMetric();
+            promptInfo.setDeviceCredentialAllowed(true);
+            biometricPrompt.authenticate(promptInfo.build());
+        });
+
     }
 
     private void insertDBticket() {
@@ -99,7 +142,70 @@ public class Fingerprint extends AppCompatActivity {
                 pendingIntent);
     }
 
+    BiometricPrompt.PromptInfo.Builder dialogMetric() {
 
+        return new BiometricPrompt.PromptInfo.Builder()
+                .setTitle("Biometric Login")
+                .setSubtitle("Login using your biometric credential");
+
+    }
+
+    void enableButton(boolean enable){
+
+        btn_fp.setEnabled(enable);
+        btn_fppin.setEnabled(true);
+    }
+
+    void enableButton(boolean enable,boolean enroll){
+
+        enableButton(enable);
+        if(!enroll) return;
+
+        Intent enrollIntent = new Intent(Settings.ACTION_BIOMETRIC_ENROLL);
+
+        enrollIntent.putExtra(Settings.EXTRA_BIOMETRIC_AUTHENTICATORS_ALLOWED,
+                BiometricManager.Authenticators.BIOMETRIC_STRONG
+                        | BiometricManager.Authenticators.BIOMETRIC_WEAK);
+
+        startActivity(enrollIntent);
+
+    }
+
+    private void checkBioMetricSupported() {
+
+        String info = "";
+
+        BiometricManager manager = BiometricManager.from(this);
+
+        switch (manager.canAuthenticate(BiometricManager.Authenticators.BIOMETRIC_WEAK
+                | BiometricManager.Authenticators.BIOMETRIC_STRONG))
+        {
+            case BiometricManager.BIOMETRIC_SUCCESS:
+                info = "App can authenticate using biometrics";
+                enableButton(true);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
+                info = "No biometric features available on this device";
+                enableButton(false);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
+                info = "Biometric features are currently unavailable";
+                enableButton(false);
+                break;
+            case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
+                info = "Need register at least one fingerprint.";
+                enableButton(false,true);
+                break;
+            default:
+                info = "Unknown cause";
+                enableButton(false);
+                break;
+        }
+
+        TextView txinfo = findViewById(R.id.tx_info);
+        txinfo.setText(info);
+
+    }
 
 
 
